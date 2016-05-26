18c59057cbc986d51133bc0b51efb32855face28
Jonathan Carroll <jono@jcarroll.com.au>
2016-05-26 22:13:54 +0930

    travis.yml added start of utils

---
+++ RDataGovAU-package.R
@@ -0,0 +1,17 @@
+#' RDataGovAU: Obtain Open Datasets From data.gov.au
+#'
+#' Uses the ckan API on data.gov.au to access, query, and 
+#' retrieve open data sets.
+#'
+#' @section internal functions:
+#' The internal yet exported functions ...
+#'
+#' @section query functions:
+#' The query functions ...
+#'
+#' @section processing functions:
+#' The processing functions ...
+#'
+#' @docType package
+#' @name RDataGovAU
+NULL
